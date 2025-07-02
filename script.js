ed5e37a17b9247eab1c6fa2fc6d6357935644a30
Ty Shaw <tyshawdesign@gmail.com>
2025-07-02 19:49:45 -0400

    Refactor section color change logic to improve readability and maintain state

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Optionally, set an id or class
 const sectionChangeButton = document.getElementById("sectionChangerButton");
-
+let isRed = false;
 if (sectionChangeButton) {
   // Add an event listener (optional)
   sectionChangeButton.addEventListener("click", () => {
@@ -13,8 +13,12 @@ if (sectionChangeButton) {
 
 function changeSectionColor(sectionID, color) {
   let section = document.getElementById(sectionID);
-  if (section) {
-    section.style.backgroundColor = color;
+  if (section && isRed === false) {
+    section.style.backgroundColor = color; // make it red
+    isRed = true; // state now is red
+  } else if (section) {
+    section.style.backgroundColor = "rgb(35, 35, 35)"; // back to default
+    isRed = false; // state now is not red
   } else {
     console.warn(`${sectionID} not found!`);
   }
