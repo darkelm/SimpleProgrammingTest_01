cd0feadba39fee0b1b81bfdfc05133aff54ce771
Ty Shaw <tyshawdesign@gmail.com>
2025-07-02 19:40:33 -0400

    Refactor button click event handling and extract color change logic into a separate function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Optionally, set an id or class
 const sectionChangeButton = document.getElementById("sectionChangerButton");
 
-// Add an event listener (optional)
-sectionChangeButton.addEventListener("click", () => {
-  alert("Button clicked!");
-  const mainSection = document.getElementById("mainSection");
-  mainSection.style.backgroundColor = "red";
-});
+if (sectionChangeButton) {
+  // Add an event listener (optional)
+  sectionChangeButton.addEventListener("click", () => {
+    alert("Button clicked!");
+    changeSectionColor("mainSection", "red");
+  });
+} else {
+  console.warn("Button is missing!");
+}
+
+function changeSectionColor(sectionID, color) {
+  let section = document.getElementById(sectionID);
+  if (section) {
+    section.style.backgroundColor = color;
+  } else {
+    console.warn(`${sectionID} not found!`);
+  }
+}
